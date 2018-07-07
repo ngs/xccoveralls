82318d4e8522f61bbf5c9f86df6adbfa9c6f9bfe
Atsushi Nagase <a@ngs.io>
2018-07-07 23:05:36 +0900

    SimpleCov filters vendor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'simplecov'
 SimpleCov.start do
   root PROJECT_ROOT_PATH
   add_filter '/spec/'
+  add_filter '/vendor/'
 end
 
 Dir[File.join(PROJECT_ROOT_PATH, 'lib', '**', '*.rb')].each do |f|
